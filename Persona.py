de35d906aef2ba4e039d0b4da8141f29f348aae1
jnanclares <73925961+jnanclares@users.noreply.github.com>
2022-10-14 21:40:38 -0500

    1

---
+++ Persona.py
@@ -0,0 +1,13 @@
+
+class Persona:
+    def __init__(self, nombre, edad, altura, sexo):
+        self._nombre = nombre
+        self._edad = edad
+        self._altura = altura
+        self._sexo = sexo
+
+
+
+        
+    
+        
