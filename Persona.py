4d70fa65059458aacf51e90fae5dd784b1773596
jnanclares <73925961+jnanclares@users.noreply.github.com>
2022-10-14 21:55:34 -0500

    attempt 1

--- Persona.py
+++ Persona.py
@@ -7,6 +7,31 @@ class Persona:
         self._sexo = sexo
 
 
+    def getNombre(self):
+        return self._nombre
+    
+    def setNombre(self, nombre):
+        self._nombre = nombre
+    
+    def getEdad(self):
+        return self._edad
+    
+    def setEdad(self, edad):
+        self._edad = edad
+    
+    def getAltura(self):
+        return self._altura
+    
+    def setAltura(self, altura):
+        self._altura = altura
+    
+    def getSexo(self):
+        return self._sexo
+    
+    def setSexo(self, sexo):
+        self._sexo = sexo
+    
+    
 
         
     
